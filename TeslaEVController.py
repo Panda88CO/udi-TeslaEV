9f1a639559aa21215fa53fefafb73fc670cbad28
Panda88CO <65139210+Panda88CO@users.noreply.github.com>
2022-09-12 14:54:37 -0700

    0.1.42

--- TeslaEVController.py
+++ TeslaEVController.py
@@ -359,7 +359,7 @@ if __name__ == "__main__":
     try:
         logging.info('Starting TeslaEV Controller')
         polyglot = udi_interface.Interface([])
-        polyglot.start('0.1.41')
+        polyglot.start('0.1.42')
         TeslaEVController(polyglot, 'controller', 'controller', 'Tesla EVs')
 
 
